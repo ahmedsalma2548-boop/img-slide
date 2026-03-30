fea3815b38539c6c1d56fe361f6b6508549d120e
Ahmed Salma <ahmed.salma2548@gmail.com>
2026-03-30 23:15:18 +0200

    reupload project

--- main.js
+++ main.js
@@ -1,102 +1,79 @@
 let slideImg = document.querySelectorAll(".slider img");
 let imgNumber = slideImg.length;
 let currentSlide = 1;
-let slideNumberelemant = document.querySelector(".slide-num");
-let prevButton = document.querySelector(".prev");
-let nextButton = document.querySelector(".next");
-let FirstSlide = document.querySelector(".button")
-let lastSlide = document.querySelector(".button-last")
-let ulElemant = document.createElement('ul');
-ulElemant.setAttribute('id', 'ulslide');
 
+let slideNum = document.querySelector(".slide-num");
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
+let firstBtn = document.querySelector(".button");
+let lastBtn = document.querySelector(".button-last");
+
+let numberBox = document.querySelector("#number");
+
+let ul = document.createElement("ul");
+numberBox.appendChild(ul);
+
+/* create bullets */
 for (let i = 1; i <= imgNumber; i++) {
-  let liElemant = document.createElement('li');
-  liElemant.setAttribute('data-index', i);
-  liElemant.appendChild(document.createTextNode(i));
-  ulElemant.appendChild(liElemant);
-}
-document.getElementById('number').appendChild(ulElemant);
-
-let imgButton = document.querySelectorAll("#ulslide li");
-
-/* تعريف الدوال */
-function removeAllactive() {
-  slideImg.forEach(img => img.classList.remove("active"));
-  imgButton.forEach(button => button.classList.remove("active"));
+    let li = document.createElement("li");
+    li.textContent = i;
+    li.dataset.index = i;
+    ul.appendChild(li);
 }
 
-// استرجاع السلايد من localStorage لو موجود
-if (localStorage.getItem("current-slide")) {
-  currentSlide = parseInt(localStorage.getItem("current-slide"), 10);
+let bullets = document.querySelectorAll("ul li");
+
+/* load saved slide */
+if (localStorage.getItem("slide")) {
+    currentSlide = Number(localStorage.getItem("slide"));
 }
 
+function updateSlider() {
+    slideImg.forEach(img => img.classList.remove("active"));
+    bullets.forEach(b => b.classList.remove("active"));
 
-function check() {
-  // أول حاجة نشيل أي active قديم
-  removeAllactive();
-  
+    slideImg[currentSlide - 1].classList.add("active");
+    bullets[currentSlide - 1].classList.add("active");
 
-  // نعرض رقم الشريحة
-  slideNumberelemant.innerHTML = `Slide #${currentSlide} Of ${imgNumber}`;
+    slideNum.textContent = `Slide ${currentSlide} / ${imgNumber}`;
 
-  // نضيف active للشريحة الحالية ولزرها
-  slideImg[currentSlide - 1].classList.add("active");
-  ulElemant.children[currentSlide - 1].classList.add("active");
-
-  // نحدّث حالة الأزرار
-  if (currentSlide === 1) {
-    prevButton.classList.add("disable");
-  } else {
-    prevButton.classList.remove("disable");
-  }
-
-  if (currentSlide === imgNumber) {
-    nextButton.classList.add("disable");
-  } else {
-    nextButton.classList.remove("disable");
-  }
-
-  localStorage.setItem("current-slide", currentSlide);
-
-
+    localStorage.setItem("slide", currentSlide);
 }
 
-/* استدعاء مبدئي */
-check();
+updateSlider();
 
-/* أحداث الأزرار */
-nextButton.onclick = function () {
-  if (currentSlide < imgNumber) {
-    currentSlide++;
-    check();
-  }
+/* next */
+nextBtn.onclick = () => {
+    if (currentSlide < imgNumber) {
+        currentSlide++;
+        updateSlider();
+    }
 };
 
-prevButton.onclick = function () {
-  if (currentSlide > 1) {
-    currentSlide--;
-    check();
-  }
+/* prev */
+prevBtn.onclick = () => {
+    if (currentSlide > 1) {
+        currentSlide--;
+        updateSlider();
+    }
 };
 
-/* (اختياري) حدث للنقر على الـ bullets عشان ينقلك لشريحة رقمية */
-imgButton.forEach(btn => { 
-  btn.addEventListener("click", (e) => {
-    currentSlide = parseInt(e.target.dataset.index, 10);
-    check();
-  });
+/* bullets */
+bullets.forEach(b => {
+    b.onclick = (e) => {
+        currentSlide = Number(e.target.dataset.index);
+        updateSlider();
+    };
 });
 
-FirstSlide.onclick = function(){
-  if(currentSlide !== 1){
-    currentSlide = 1
-    check()
-  }
-}
-lastSlide.onclick = function(){
-  if(currentSlide < imgNumber){
-    currentSlide = slideImg.length
-    check()
+/* first */
+firstBtn.onclick = () => {
+    currentSlide = 1;
+    updateSlider();
+};
 
-  }
-}
+/* last */
+lastBtn.onclick = () => {
+    currentSlide = imgNumber;
+    updateSlider();
+};
